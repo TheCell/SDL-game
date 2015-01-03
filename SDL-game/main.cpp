23b8f4e1f974d6b54315c310bd6e68fc93a461ad
TheCell <simon_hischier@hotmail.com>
2015-01-03 10:45:18 +0100

    Code breakdown and extended
    SDL Code now in Game.h / Game.cpp
    main function without sdl specific code

--- SDL-game/main.cpp
+++ SDL-game/main.cpp
@@ -1,64 +1,33 @@
-#include<SDL.h>;
+#include "Game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false; // this will create a loop
+// remove when release
+//#include "Windows.h"
+#include <iostream>
+// end of remove when release
 
-bool init(const char* title, int xpos, int ypos, int
-	height, int width, int flags)
+// our Game object
+Game* g_game = 0;
+
+int main(int argc, char* argv[])
 {
-	// initialize SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	// std::cout << "test";
+	// remove when release
+	//AllocConsole();
+	//freopen("CON", "w", stdout);
+	// end of remove when release
+
+	g_game = new Game();
+
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
+
+	while (g_game->running())
 	{
-		// if succeeded create our window
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos,
-			height, width, flags);
-
-		// if the window creation succeeded create our renderer
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
-	else
-	{
-		return false; // sdl could not initialize
-	}
-
-	return true;
-}
-
-void render()
-{
-	// set to black
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-	// clear the window to black
-	SDL_RenderClear(g_pRenderer);
-
-	// show the window
-	SDL_RenderPresent(g_pRenderer);
-}
-
-int main(int argc, char* args[])
-{
-	if (init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN))
-	{
-		g_bRunning = true;
-	}
-	else
-	{
-		return 1; // something's wrong
-	}
-
-	while (g_bRunning)
-	{
-		render();
-	}
-
-	// clean up SDL
-	SDL_Quit();
+	
+	g_game->clean();
 
 	return 0;
 }
